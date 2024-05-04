141dc5202d7f6a2d3625d7714330121adc272778
andrei <andrei.taraschuk@gmail.com>
2024-05-04 16:56:57 -0600

    Updated solidity version

--- src/BBitsSocial.sol
+++ src/BBitsSocial.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.25;
 
 import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import {Pausable} from "lib/openzeppelin-contracts/contracts/utils/Pausable.sol";
