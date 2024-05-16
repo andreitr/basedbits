9c230015fb7d23cf7b6b1e666c290dcb774b98e9
andrei <andrei.taraschuk@gmail.com>
2024-05-15 20:29:30 -0600

    ERC1155 implementation

--- test/BBitsCheckInBadges.sol
+++ test/BBitsCheckInBadges.sol
@@ -5,6 +5,7 @@ import "forge-std/Test.sol";
 import "../src/BBitsCheckInBadges.sol";
 
 contract BBitsCheckInBadgesTest is Test {
+
     BBitsCheckInBadges private badges;
     address private owner;
     address private minter;
@@ -27,6 +28,12 @@ contract BBitsCheckInBadgesTest is Test {
         assertEq(badges.balanceOf(user, 1), 100);
     }
 
+    function testBalanceOf() public {
+        vm.prank(minter);
+        badges.mint(user, 1, 100, "");
+        assertEq(badges.balanceOf(user, 1), 100);
+    }
+
     function testMintBatch() public {
         uint256[] memory ids = new uint256[](2);
         uint256[] memory amounts = new uint256[](2);
@@ -42,31 +49,31 @@ contract BBitsCheckInBadgesTest is Test {
         assertEq(badges.balanceOf(user, 2), 200);
     }
 
+    function testFailMintBatchIfNotMinter() public {
+        uint256[] memory ids = new uint256[](2);
+        uint256[] memory amounts = new uint256[](2);
+        ids[0] = 1;
+        ids[1] = 2;
+        amounts[0] = 100;
+        amounts[1] = 200;
+
+        vm.prank(user);
+        badges.mintBatch(user, ids, amounts, "");
+    }
+
     function testSetTokenURI() public {
         vm.prank(owner);
         badges.setTokenURI(1, "https://api.example.com/metadata/1");
         assertEq(badges.uri(1), "https://api.example.com/metadata/1");
     }
 
-    function testSetTokenURIFailsIfNotAdmin() public {
+    function testFailSetTokenURIIfNotAdmin() public {
         vm.prank(user);
-        vm.expectRevert("AccessControl: account is missing role");
-
         badges.setTokenURI(1, "https://api.example.com/metadata/1");
     }
 
-    function testMintFailsIfNotMinter() public {
+    function testFailMintIfNotMinter() public {
         vm.prank(user);
-        vm.expectRevert("AccessControl: account is missing role");
-
         badges.mint(user, 1, 100, "");
     }
-
-//    function testSupportsInterface() public {
-//        bool supportsERC1155 = badges.supportsInterface(type(IERC1155).interfaceId);
-//        bool supportsAccessControl = badges.supportsInterface(type(IAccessControl).interfaceId);
-//
-//        assertTrue(supportsERC1155);
-//        assertTrue(supportsAccessControl);
-//    }
 }
