39d9d7d7f34a30ec02ccd038020d65d4568d24ad
andrei <andrei.taraschuk@gmail.com>
2024-05-18 13:11:10 -0600

    Minor optimizations on the badges contract.

--- src/BBitsBadges.sol
+++ src/BBitsBadges.sol
@@ -6,7 +6,7 @@ import {ERC1155Supply} from "lib/openzeppelin-contracts/contracts/token/ERC1155/
 import {ERC1155URIStorage} from "lib/openzeppelin-contracts/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
 import {AccessControl} from "lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
 
-contract BBitsCheckInBadges is ERC1155, ERC1155Supply, ERC1155URIStorage, AccessControl {
+contract BBitsBadges is ERC1155, ERC1155Supply, ERC1155URIStorage, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor(string memory baseURI) ERC1155(baseURI) {
@@ -24,22 +24,14 @@ contract BBitsCheckInBadges is ERC1155, ERC1155Supply, ERC1155URIStorage, Access
         return super.supportsInterface(interfaceId);
     }
 
-    // Mint function to create new tokens
-    function mint(address to, uint256 id, uint256 amount, bytes memory data) public onlyRole(MINTER_ROLE) {
+    function mint(address to, uint256 id, uint256 amount, bytes memory data) external onlyRole(MINTER_ROLE) {
         _mint(to, id, amount, data);
     }
 
-    // MintBatch function to create new tokens in batch
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) public onlyRole(MINTER_ROLE) {
-        _mintBatch(to, ids, amounts, data);
-    }
-
-    // Function to set the URI for a specific token (only admin can do this)
-    function setTokenURI(uint256 tokenId, string memory newuri) public onlyRole(DEFAULT_ADMIN_ROLE) {
+    function setTokenURI(uint256 tokenId, string memory newuri) external onlyRole(DEFAULT_ADMIN_ROLE) {
         _setURI(tokenId, newuri);
     }
 
-    // Override the uri function to return the specific URI for each token
     function uri(uint256 tokenId) public view override(ERC1155, ERC1155URIStorage) returns (string memory) {
         return ERC1155URIStorage.uri(tokenId);
     }
--- test/BBitsBadges.sol
+++ test/BBitsBadges.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
-import "../src/BBitsCheckInBadges.sol";
+import "../src/BBitsBadges.sol";
 
 contract BBitsCheckInBadgesTest is Test {
 
-    BBitsCheckInBadges private badges;
+    BBitsBadges private badges;
     address private owner;
     address private minter;
     address private user;
@@ -16,7 +16,7 @@ contract BBitsCheckInBadgesTest is Test {
         minter = address(0x1);
         user = address(0x2);
 
-        badges = new BBitsCheckInBadges("https://api.example.com/metadata/");
+        badges = new BBitsBadges("https://api.example.com/metadata/");
 
         badges.grantRole(badges.MINTER_ROLE(), minter);
     }
@@ -32,33 +32,6 @@ contract BBitsCheckInBadgesTest is Test {
         vm.prank(minter);
         badges.mint(user, 1, 100, "");
         assertEq(badges.balanceOf(user, 1), 100);
-    }
-
-    function testMintBatch() public {
-        uint256[] memory ids = new uint256[](2);
-        uint256[] memory amounts = new uint256[](2);
-        ids[0] = 1;
-        ids[1] = 2;
-        amounts[0] = 100;
-        amounts[1] = 200;
-
-        vm.prank(minter);
-        badges.mintBatch(user, ids, amounts, "");
-
-        assertEq(badges.balanceOf(user, 1), 100);
-        assertEq(badges.balanceOf(user, 2), 200);
-    }
-
-    function testFailMintBatchIfNotMinter() public {
-        uint256[] memory ids = new uint256[](2);
-        uint256[] memory amounts = new uint256[](2);
-        ids[0] = 1;
-        ids[1] = 2;
-        amounts[0] = 100;
-        amounts[1] = 200;
-
-        vm.prank(user);
-        badges.mintBatch(user, ids, amounts, "");
     }
 
     function testSetTokenURI() public {
