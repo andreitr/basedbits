52a6470a999945cb268ff485a7ea2e205f9f86ec
andreitr <andrei@mac.lan>
2025-05-30 20:06:32 -0600

    Fixed misc issue

--- script/aeye/AEYEDeploy.s.sol
+++ script/aeye/AEYEDeploy.s.sol
@@ -6,6 +6,7 @@ import {AEYE} from "@src/AEYE.sol";
 
 contract AEYEDeploy is Script {
         
+    AEYE public aeye;
     address public creator = 0x1d671d1B191323A38490972D58354971E5c1cd2A;
     address public burner = 0x1595409cbAEf3dD2485107fb1e328fA0fA505c10;
 
