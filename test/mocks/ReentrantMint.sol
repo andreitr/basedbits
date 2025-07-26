0558f0df06e8f9a5d5120f428ea01d52896f4f2d
andrei <andrei.taraschuk@gmail.com>
2025-07-26 06:36:02 -0600

    Update PotRaider burn to use burner interface

--- test/mocks/ReentrantMint.sol
+++ test/mocks/ReentrantMint.sol
@@ -3,7 +3,11 @@ pragma solidity 0.8.25;
 
 import {PotRaider} from "@src/PotRaider.sol";
 
-contract ReentrantMint {
+interface Burner {
+    function burn(uint256 _minAmountBurned) external payable;
+}
+
+contract ReentrantMint is Burner {
     PotRaider public potRaider;
     bool public reentered;
 
@@ -13,6 +17,13 @@ contract ReentrantMint {
 
     function attack() external payable {
         potRaider.mint{value: msg.value}(1);
+    }
+
+    function burn(uint256) external payable {
+        if (!reentered) {
+            reentered = true;
+            potRaider.mint{value: potRaider.mintPrice()}(1);
+        }
     }
 
     receive() external payable {
