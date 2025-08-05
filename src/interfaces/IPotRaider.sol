6e06f8a1735ed017ffa97795f6571293da8e1c11
andrei <andrei.taraschuk@gmail.com>
2025-08-04 21:46:18 -0600

    refactor: simplify PotRaider payout

--- src/interfaces/IPotRaider.sol
+++ src/interfaces/IPotRaider.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.25;
 interface IPotRaider {
     event NFTExchanged(uint256 indexed tokenId, address indexed owner, uint256 ethAmount, uint256 usdcAmount);
     event LotteryTicketPurchased(uint256 indexed day, uint256 amount);
-    event PercentagesUpdated(uint256 burnPercentage, uint256 artistPercentage);
+    event BurnPercentageUpdated(uint256 burnPercentage);
     event LotteryReferrerUpdated(address indexed newReferrer);
     event MintPriceUpdated(uint256 _mintPrice);
     event LotteryParticipationDaysUpdated(uint256 _lotteryParticipationDays);
